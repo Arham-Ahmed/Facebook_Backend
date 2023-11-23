7a8b606b4f2414d5c6b5220507c2692e0449829b
Arham <arham-ahmed@iinv.tech>
2023-11-23 14:52:43 +0500

    adding mongodb atlas

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const ConnectDb = (uri) => {
   console.log("\x1b[33m", "Connecting to DataBase");
-  return mongoose.connect("mongodb://localhost:27017/facebook");
+  // return mongoose.connect("mongodb://localhost:27017/facebook");
+  return mongoose.connect(process.env.MONGODBURL);
 };
 
 module.exports = ConnectDb;
